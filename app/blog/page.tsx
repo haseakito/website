d1269f985a2c20088a7c074bef48693aaeec3bb8
haseakito <haseakito@gmail.com>
2023-08-06 20:31:06 +0800

    :lipstick: Update Ul for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,6 +8,8 @@ import { Blog } from '@/utils/types/Blog'
 import { Tag } from '@/utils/types/Tag'
 import Image from 'next/image'
 import Coffee from '@/public/Coffee.svg'
+import Camera from '@/public/Camera.svg'
+import Cocktail from '@/public/Cocktail.svg'
 import { Tweet } from 'react-tweet'
 
 // Function handling fetching the blogs from microCMS
@@ -38,15 +40,45 @@ export default async function page() {
 
   return (
     <PageTransitionAnimation>
-        <div className='h-screen'>
+        <div className='h-full'>
             <Navigation
                 path='Blogs'
             />
+            <section className='relative flex justify-center bg-white dark:bg-slate-800 my-20 px-6'>
+                <div className='w-full h-full absolute animate-fade-in ease-in duration-[2000ms] delay-1000'>
+                    <div className='absolute flex justify-center items-center w-full h-full animate-fade-in ease-in'>
+                        <Image
+                            src={ Coffee }
+                            alt=''
+                            className='-rotate-12 w-[30vw] absolute right-0 -bottom-0 lg:-bottom-10 [@media(min-width: 2160px)]:-bottom-48 -translate-x-16'
+                        />
+                        <Image
+                            src={ Camera }
+                            alt=''
+                            className='rotate-6 w-[30vw] lg:w-[25vw] absolute left-0 -top-16 translate-x-16 animate-fade-in'
+                        />
+                        <span className='animate-fade-in rotate-12 bg-pink-300 rounded-xl h-72 w-40 lg:w-52 absolute -top-16 right-0 translate-x-36 transform hover:translate-x-28 duration-300'></span>
+                        <span className='animate-fade-in rotate-12 bg-sky-300 rounded-xl h-72 w-40 lg:w-52 absolute -bottom-28 left-0 -translate-x-36 transform hover:-translate-x-28 duration-300'></span>
+                    </div>
+                </div>
+                <div className='relative inline-block text-center my-60 md:my-48 xl:my-64 animate-fade-in ease-out duration-500 delay-300'>
+                    <div className='animate-flip-in ease-out duration-[1500ms]'>
+                        <h1 className='mb-4 font-extrabold text-2xl md:text-4xl animate-fade-in'>
+                            <span className='text-amber-300 hover:text-amber-400 duration-300'>What</span> is <span className='text-purple-300 hover:text-purple-400 duration-300'>new</span>?
+                        </h1>
+                        <h4 className='text-lg font-semibold'>
+                            Latest updates from a corner of the world
+                        </h4>
+                        <span className='absolute bg-black w-48 h-1 left-1/2 bottom-0 animate-scale-in-center'></span>
+                        <span className='absolute bg-black w-48 h-1 right-1/2 bottom-0 animate-scale-in-center'></span>
+                    </div>
+                </div>
+            </section>
             <div className='flex justify-center'>
                 <div className='relative inline-block'>
                     <div className='flex'>                        
                         <Image
-                            src={ Coffee }
+                            src={ Cocktail }
                             alt=''
                             width={ 70 }
                             className=''
