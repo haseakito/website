51ad03db3ade84fc482185a77104f8dfdf71d4da
haseakito <haseakito@gmail.com>
2026-05-03 21:09:19 +0900

    feat: add rss feed generation

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,28 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "@/config/site";
+
+export async function GET(context) {
+  let items = [];
+
+  const posts = await getCollection("articles");
+  items = posts.map((post) => ({
+    ...post.data,
+    link: `/articles/${post.slug}/`,
+  }));
+
+  const pages = await getCollection("other");
+  items = items.concat(
+    pages.map((post) => ({
+      ...post.data,
+      link: `/${post.slug}/`,
+    })),
+  );
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    items: items,
+    site: context.site,
+  });
+}
